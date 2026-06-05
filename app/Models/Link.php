7035378120b32fe9baea743093bbc0f9cc6207a3
SergeyYashnik <fggsshugvgdcj@gmail.com>
2026-06-05 20:20:47 +0500

    Create links table migration and Link model with indexes

---
+++ app/Models/Link.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    protected $fillable = [
+        'url',
+        'code',
+        'clicks',
+    ];
+
+    protected $casts = [
+        'clicks' => 'integer',
+    ];
+}
